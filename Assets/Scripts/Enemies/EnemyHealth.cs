e03440eb066673410c9618668a8aee7815be9545
RossTubesGames <jordanross1414@gmail.com>
2026-01-29 14:49:46 +0100

    added a hit marker script that instanciated the image infront of the player

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 using FMOD;
 public class EnemyHealth : MonoBehaviour,Arrows.IDamageable
 {
-
     [SerializeField] private float health = 30f;
+
+    private HitMarker hitMarker;
+
+    private void Awake()
+    {
+        hitMarker = FindFirstObjectByType<HitMarker>();
+    }
 
     public void TakeDamage(float damage)
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Cultist");
         health -= damage;
+
+        if (hitMarker != null)
+            hitMarker.Show();
 
         if (health <= 0f)
             Destroy(gameObject);
